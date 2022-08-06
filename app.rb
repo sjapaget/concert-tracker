a79a54d98fc77e98b34b91efdd6338fae6f69412
sam <sjapaget@gmail.com>
2022-08-06 16:39:39 +0200

    Add artists section to index

--- app.rb
+++ app.rb
@@ -3,13 +3,16 @@ require "sinatra/reloader" if development?
 require "sinatra/activerecord"
 require "pry-byebug"
 require "better_errors"
+
 require_relative "config/application"
 require_relative "models/venue"
+require_relative "models/artist"
 
 
 get "/" do
 
   @all_venues = Venue.all
+  @all_artists = Artist.all
 
   erb :index
 end
